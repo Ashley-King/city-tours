e1e89fb4239a08b6881ba5ee69c42d98547bb156
Ashley King <ashleyot@gmail.com>
2019-05-16 16:45:57 -0400

    ready to deploy to netlify

---
+++ src/components/NavBar/NavBar.js
@@ -0,0 +1,28 @@
+import React from "react";
+import "./navbar.scss";
+import logo from "../../logo.svg";
+
+export default function NavBar() {
+  return (
+    <nav className="navbar">
+      <img src={logo} alt="City Tours Company" />
+      <ul className="nav-links">
+        <li>
+          <a href="/" className="nav-link">
+            Home
+          </a>
+        </li>
+        <li>
+          <a href="/" className="nav-link">
+            About
+          </a>
+        </li>
+        <li>
+          <a href="/" className="nav-link active">
+            Tours
+          </a>
+        </li>
+      </ul>
+    </nav>
+  );
+}
